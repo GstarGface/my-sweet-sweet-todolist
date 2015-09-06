f10bed904cb23c76def6b45f62679f2a30f3be0c
Georgia Glassie <georgia@georgia-mac.local>
2015-09-06 13:13:15 -0700

    forms to dynamically generate todo items

--- simple-todos.js
+++ simple-todos.js
@@ -5,7 +5,38 @@ if (Meteor.isClient) {
 
   Template.body.helpers({
     tasks: function(){
-      return Tasks.find({});
+      return Tasks.find({}, {sort: {createdAt:-1}});
+    }
+  });
+
+  Template.body.events({
+    "submit .new-task": function(event){
+      //Prevent default browser form action
+      event.preventDefault();
+
+      //Get value from text form element
+      var text = event.target.text.value;
+
+      //Insert a teask into the collection
+      Tasks.insert({
+        text: text,
+        createdAt:new Date() //current timestamp
+      });
+
+      //Clear form 
+      event.target.text.value = "";
+
+    }
+  });
+  Template.task.events({
+    "click .toggle-checked": function(){
+      //Toggles the checked property
+      Tasks.update(this._id, {
+        $set: {checked: ! this.checked}
+      });
+    },
+    "click .delete": function() {
+      Tasks.remove(this._id);
     }
   });
 }
